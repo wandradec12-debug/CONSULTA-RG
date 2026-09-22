86f35955208ca71c57e8905b0d694a7f9504c3b1
wandradec12-debug <wandrade.c12@gmail.com>
2026-09-22 08:34:13 -0300

    Incluir leitor QR no cache offline

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,5 +1,5 @@
 const CACHE='consulta-rg-offline-v3';
-const CORE=['/static/offline.html','/static/style.css','/static/manifest.json','/static/icon-192.svg','/static/icon-512.svg'];
+const CORE=['/static/offline.html','/static/style.css','/static/manifest.json','/static/icon-192.svg','/static/icon-512.svg','/static/jsQR.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
